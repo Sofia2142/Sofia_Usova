f8ccbbe95ff99c5dc80d1a48cd10e7736f2a5826
Sofia2142 <109218634+Sofia2142@users.noreply.github.com>
2022-07-15 16:52:06 +0300

    Add files via upload

---
+++ shedule_bot/buttons.py
@@ -0,0 +1,5 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+user_kb = ReplyKeyboardMarkup(resize_keyboard=True)\
+    .row(KeyboardButton('Пн'), KeyboardButton('Вт'), KeyboardButton('Ср'), KeyboardButton('Чт'))\
+    .row(KeyboardButton('Пт'), KeyboardButton('Сб'), KeyboardButton('Сегодня'), KeyboardButton('Завтра'))
